abf7ea5beebeca3f33c44514e6de3a59a14299c7
1badnews <aras.butrimanskas@gmail.com>
2022-03-07 00:16:55 +0200

    fixed register errors

--- app/src/main/java/com/example/lostandfound/RegisterActivity.java
+++ app/src/main/java/com/example/lostandfound/RegisterActivity.java
@@ -2,12 +2,14 @@ package com.example.lostandfound;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -52,7 +54,29 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
         String password=registerPassword.getText().toString().trim();
         String password2=registerPassword2.getText().toString().trim();
 
+
         if (email.isEmpty()){
+
+            registerEmail.setError("Email is empty!");
+            registerEmail.requestFocus();
+        }
+
+        if (password.isEmpty()){
+            registerPassword.setError("Password is empty!");
+            registerPassword.requestFocus();
+        }
+
+        if (password2.isEmpty()){
+            registerPassword2.setError("Confirm Password is empty!");
+            registerPassword2.requestFocus();
+
+        }
+        if (!registerPassword.getText().toString().equals(registerPassword2.getText().toString()))
+        {
+            registerPassword2.setError("Passwords do not match!");
+            registerPassword.setError("Passwords do not match!");
+            registerPassword.requestFocus();
+
 
         }
     }
