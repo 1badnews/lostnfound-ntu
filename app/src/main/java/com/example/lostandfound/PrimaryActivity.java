1c740b1182d575ebe6273b6ea765a46c40ddbb15
1badnews <aras.butrimanskas@gmail.com>
2022-03-10 17:52:26 +0200

    nav v1

--- app/src/main/java/com/example/lostandfound/PrimaryActivity.java
+++ app/src/main/java/com/example/lostandfound/PrimaryActivity.java
@@ -1,14 +1,39 @@
 package com.example.lostandfound;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class PrimaryActivity extends AppCompatActivity {
 
+    private DrawerLayout draw;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primary);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        draw=findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, draw,
+                toolbar, R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        draw.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (draw.isDrawerOpen(GravityCompat.START)){
+            draw.closeDrawer(GravityCompat.START);
+        } else{
+            super.onBackPressed();
+        }
     }
 }
