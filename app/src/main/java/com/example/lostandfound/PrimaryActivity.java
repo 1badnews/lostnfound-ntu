e040783b36b08a0c5d40ef3d9dae0a83aed41705
1badnews <aras.butrimanskas@gmail.com>
2022-03-10 18:54:53 +0200

    fragments fully implemented (may add more later)

--- app/src/main/java/com/example/lostandfound/PrimaryActivity.java
+++ app/src/main/java/com/example/lostandfound/PrimaryActivity.java
@@ -1,5 +1,6 @@
 package com.example.lostandfound;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -7,8 +8,11 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class PrimaryActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class PrimaryActivity extends AppCompatActivity  implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout draw;
 
@@ -21,11 +25,36 @@ public class PrimaryActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         draw=findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, draw,
                 toolbar, R.string.navigation_drawer_open,
                 R.string.navigation_drawer_close);
         draw.addDrawerListener(toggle);
         toggle.syncState();
+        if (savedInstanceState==null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LostItemsFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_lostitems);}
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.nav_lostitems:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new LostItemsFragment()).commit();
+
+                break;
+            case R.id.nav_myitems:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new MyItemsFragment()).commit();
+
+                break;
+            case R.id.nav_postitem:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PostItemFragment()).commit();
+
+                break;
+        }
+        draw.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
