c377e4e928a9a96ed8cab755010bcad1d6426200
1badnews <aras.butrimanskas@gmail.com>
2022-03-12 17:21:46 +0200

    implemented my items query

--- app/src/main/java/com/example/lostandfound/MyItemsFragment.java
+++ app/src/main/java/com/example/lostandfound/MyItemsFragment.java
@@ -8,11 +8,49 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MyItemsFragment extends Fragment {
+    RecyclerView recyclerview;
+    ItemAdapter itemadapter;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_myitems,container,false);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String email = user.getEmail();
+        View retrn = inflater.inflate(R.layout.fragment_myitems,container,false);
+        recyclerview= retrn.findViewById(R.id.itemlist);
+        recyclerview.setLayoutManager(new LinearLayoutManager(requireContext()));
+
+        FirebaseRecyclerOptions<Items> options = new FirebaseRecyclerOptions.Builder<Items>()
+                .setQuery(FirebaseDatabase.getInstance().getReference().child("Items").orderByChild("user").equalTo(email), Items.class )
+                .build();
+
+        itemadapter = new ItemAdapter(options);
+        recyclerview.setAdapter(itemadapter);
+        return retrn;
+
     }
-}
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        itemadapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        itemadapter.stopListening();
+    }
+
+    }
+
